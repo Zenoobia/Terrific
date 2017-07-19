50d2151dbafbe01619af69f7a661d5fa3e6f7071
Zenoobia <joaqimpla@gmail.com>
2017-07-20 01:27:26 +0200

    N/A

--- src/test.cpp
+++ src/test.cpp
@@ -1,24 +1,34 @@
+#include <sol.hpp>
+#include <cassert>
+
 #include <ImGui/imgui.h>
 #include <ImGui/imgui-SFML.h>
- 
+
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/System/Clock.hpp>
 #include <SFML/Window/Event.hpp>
- 
+
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(640, 480), "");
     window.setVerticalSyncEnabled(true);
     ImGui::SFML::Init(window);
- 
+
+    // Sol initialization
+    sol::state lua;
+    int x = 0;
+    lua.set_function("beep", [&x]{ ++x; });
+    lua.script("beep()");
+    assert(x == 1);
+
     sf::Color bgColor;
- 
+
     float color[3] = { 0.f, 0.f, 0.f };
- 
+
     // let's use char array as buffer, see next part
     // for instructions on using std::string with ImGui
     char windowTitle[255] = "ImGui + SFML = <3";
- 
+
     window.setTitle(windowTitle);
     window.resetGLStates(); // call it if you only draw ImGui. Otherwise not needed.
     sf::Clock deltaClock;
@@ -26,16 +36,16 @@ int main()
         sf::Event event;
         while (window.pollEvent(event)) {
             ImGui::SFML::ProcessEvent(event);
- 
+
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
         }
- 
+
         ImGui::SFML::Update(window, deltaClock.restart());
- 
+
         ImGui::Begin("Sample window"); // begin window
- 
+
                                        // Background color edit
         if (ImGui::ColorEdit3("Background color", color)) {
             // this code gets called if color value changes, so
@@ -44,10 +54,10 @@ int main()
             bgColor.g = static_cast<sf::Uint8>(color[1] * 255.f);
             bgColor.b = static_cast<sf::Uint8>(color[2] * 255.f);
         }
- 
+
         // Window title text edit
         ImGui::InputText("Window title", windowTitle, 255);
- 
+
         if (ImGui::Button("Update window title")) {
             // this code gets if user clicks on the button
             // yes, you could have written if(ImGui::InputText(...))
@@ -55,11 +65,11 @@ int main()
             window.setTitle(windowTitle);
         }
         ImGui::End(); // end window
- 
+
         window.clear(bgColor); // fill background with color
         ImGui::Render();
         window.display();
     }
- 
+
     ImGui::SFML::Shutdown();
 }
